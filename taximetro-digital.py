4167938df16e49243db7f2442a9d47f50c5b38df
isrodam <rodriguezamorisabel@gmail.com>
2025-12-01 20:49:47 +0100

    Explicacion uso del programa #7

--- taximetro-digital.py
+++ taximetro-digital.py
@@ -1,2 +1,4 @@
-
-print("Bienvenido al Taximetro Digital")
+print("Bienvenido al taxímetro digital")
+print("Su uso es muy sencillo. El precio por segundo detenido son 0,02 € y en movimiento 0,05 €")
+print("Solo hay que indiciar el número de segundos en los que el coche permanece detenido")
+print("Y el numero de segundos en los que el coche se ha estado moviendo durante el viaje")
